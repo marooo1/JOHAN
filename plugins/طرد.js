e8694d1a1569d29c58a251389db9dde55e116e24
marooo1 <jackalkper@gmail.com>
2024-06-06 00:50:22 +0300

    Update طرد.js

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -1,13 +1,13 @@
 
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-let kickte = `*وين شخص الي راح ينشات!*`
+let kickte = `*فين اللي هتمنشنلو؟!*`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*طردك فحلك روح ابكي عند امك ترجعك 😲!*`) 
+m.reply(`*انطر برا يكسمك م ناقصه قرف ✨!*`) 
 
 }
 
